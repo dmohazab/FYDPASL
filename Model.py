73d1f468fabe6b03eb9884a947fba2e0b10ca55f
dmohazab <dmohazab@gmail.com>
2022-01-14 19:29:18 -0500

    separated into files and saved full model

---
+++ Model.py
@@ -0,0 +1,67 @@
+from sklearn.metrics import multilabel_confusion_matrix, accuracy_score
+from tensorflow.keras.callbacks import TensorBoard
+from tensorflow.keras.layers import LSTM, Dense
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.utils import to_categorical
+from sklearn.model_selection import train_test_split
+import cv2
+import matplotlib
+import numpy as np
+import os
+from matplotlib import pyplot as plt
+import time
+
+# Path for exported data, numpy arrays
+DATA_PATH = os.path.join('MP_Data')
+
+# Actions that we try to detect
+actions = np.array(['hello', 'no'])
+
+# Thirty videos worth of data
+no_sequences = 30
+
+# Videos are going to be 30 frames in lengh
+sequence_length = 30
+
+label_map = {label: num for num, label in enumerate(actions)}
+
+sequences, labels = [], []
+for action in actions:
+    for sequence in range(no_sequences):
+        window = []
+        for frame_num in range(sequence_length):
+            res = np.load(os.path.join(DATA_PATH, action, str(
+                sequence), "{}.npy".format(frame_num)))
+            window.append(res)
+        sequences.append(window)
+        labels.append(label_map[action])
+
+X = np.array(sequences)
+y = to_categorical(labels).astype(int)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.05)
+
+log_dir = os.path.join('Logs')
+tb_callback = TensorBoard(log_dir=log_dir)
+model = Sequential()
+model.add(LSTM(64, return_sequences=True,
+          activation='relu', input_shape=(30, 1662)))
+model.add(LSTM(128, return_sequences=True, activation='relu'))
+model.add(LSTM(64, return_sequences=False, activation='relu'))
+model.add(Dense(64, activation='relu'))
+model.add(Dense(32, activation='relu'))
+model.add(Dense(actions.shape[0], activation='softmax'))
+
+res = [.7, 0.2, 0.1]
+
+model.compile(optimizer='Adam', loss='categorical_crossentropy',
+              metrics=['categorical_accuracy'])
+model.fit(X_train, y_train, epochs=500, callbacks=[tb_callback])
+
+res = model.predict(X_test)
+
+model.save('action')
+
+# del model
+
+# model = load_model('action')
+# print(model.summary())
