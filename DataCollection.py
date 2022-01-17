d3e577dfce63774404d5cdf49b312f9b2a567a78
dmohazab <dmohazab@gmail.com>
2022-01-17 15:05:28 -0500

    remove face

--- DataCollection.py
+++ DataCollection.py
@@ -20,8 +20,8 @@ def mediapipe_detection(image, model):
 
 
 def draw_landmarks(image, results):
-    mp_drawing.draw_landmarks(image, results.face_landmarks,
-                              mp_holistic.FACEMESH_TESSELATION)  # Draw face connections
+    # mp_drawing.draw_landmarks(image, results.face_landmarks,
+    #                           mp_holistic.FACEMESH_TESSELATION)  # Draw face connections
     mp_drawing.draw_landmarks(image, results.pose_landmarks,
                               mp_holistic.POSE_CONNECTIONS)  # Draw pose connections
     mp_drawing.draw_landmarks(image, results.left_hand_landmarks,
@@ -32,12 +32,12 @@ def draw_landmarks(image, results):
 
 def draw_styled_landmarks(image, results):
     # Draw face connections
-    mp_drawing.draw_landmarks(image, results.face_landmarks, mp_holistic.FACEMESH_TESSELATION,
-                              mp_drawing.DrawingSpec(
-                                  color=(80, 110, 10), thickness=1, circle_radius=1),
-                              mp_drawing.DrawingSpec(
-                                  color=(80, 256, 121), thickness=1, circle_radius=1)
-                              )
+    # mp_drawing.draw_landmarks(image, results.face_landmarks, mp_holistic.FACEMESH_TESSELATION,
+    #                           mp_drawing.DrawingSpec(
+    #                               color=(80, 110, 10), thickness=1, circle_radius=1),
+    #                           mp_drawing.DrawingSpec(
+    #                               color=(80, 256, 121), thickness=1, circle_radius=1)
+    #                           )
     # Draw pose connections
     mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS,
                               mp_drawing.DrawingSpec(
@@ -63,19 +63,19 @@ def draw_styled_landmarks(image, results):
 def extract_keypoints(results):
     pose = np.array([[res.x, res.y, res.z, res.visibility] for res in results.pose_landmarks.landmark]).flatten(
     ) if results.pose_landmarks else np.zeros(33*4)
-    face = np.array([[res.x, res.y, res.z] for res in results.face_landmarks.landmark]).flatten(
-    ) if results.face_landmarks else np.zeros(468*3)
+    # face = np.array([[res.x, res.y, res.z] for res in results.face_landmarks.landmark]).flatten(
+    # ) if results.face_landmarks else np.zeros(468*3)
     lh = np.array([[res.x, res.y, res.z] for res in results.left_hand_landmarks.landmark]).flatten(
     ) if results.left_hand_landmarks else np.zeros(21*3)
     rh = np.array([[res.x, res.y, res.z] for res in results.right_hand_landmarks.landmark]).flatten(
     ) if results.right_hand_landmarks else np.zeros(21*3)
-    return np.concatenate([pose, face, lh, rh])
+    return np.concatenate([pose, lh, rh])
 
 # Path for exported data, numpy arrays
 DATA_PATH = os.path.join('MP_Data')
 
 # Actions that we try to detect
-actions = np.array(['-'])
+actions = np.array(['Hello'])
 
 # Thirty videos worth of data
 no_sequences = 30
--- TestModel.py
+++ TestModel.py
@@ -23,8 +23,8 @@ def mediapipe_detection(image, model):
 
 
 def draw_landmarks(image, results):
-    mp_drawing.draw_landmarks(image, results.face_landmarks,
-                              mp_holistic.FACEMESH_TESSELATION)  # Draw face connections
+    # mp_drawing.draw_landmarks(image, results.face_landmarks,
+    #                           mp_holistic.FACEMESH_TESSELATION)  # Draw face connections
     mp_drawing.draw_landmarks(image, results.pose_landmarks,
                               mp_holistic.POSE_CONNECTIONS)  # Draw pose connections
     mp_drawing.draw_landmarks(image, results.left_hand_landmarks,
@@ -34,13 +34,13 @@ def draw_landmarks(image, results):
 
 
 def draw_styled_landmarks(image, results):
-    # Draw face connections
-    mp_drawing.draw_landmarks(image, results.face_landmarks, mp_holistic.FACEMESH_TESSELATION,
-                              mp_drawing.DrawingSpec(
-                                  color=(80, 110, 10), thickness=1, circle_radius=1),
-                              mp_drawing.DrawingSpec(
-                                  color=(80, 256, 121), thickness=1, circle_radius=1)
-                              )
+    # # Draw face connections
+    # mp_drawing.draw_landmarks(image, results.face_landmarks, mp_holistic.FACEMESH_TESSELATION,
+    #                           mp_drawing.DrawingSpec(
+    #                               color=(80, 110, 10), thickness=1, circle_radius=1),
+    #                           mp_drawing.DrawingSpec(
+    #                               color=(80, 256, 121), thickness=1, circle_radius=1)
+    #                           )
     # Draw pose connections
     mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS,
                               mp_drawing.DrawingSpec(
@@ -66,13 +66,13 @@ def draw_styled_landmarks(image, results):
 def extract_keypoints(results):
     pose = np.array([[res.x, res.y, res.z, res.visibility] for res in results.pose_landmarks.landmark]).flatten(
     ) if results.pose_landmarks else np.zeros(33*4)
-    face = np.array([[res.x, res.y, res.z] for res in results.face_landmarks.landmark]).flatten(
-    ) if results.face_landmarks else np.zeros(468*3)
+    # face = np.array([[res.x, res.y, res.z] for res in results.face_landmarks.landmark]).flatten(
+    # ) if results.face_landmarks else np.zeros(468*3)
     lh = np.array([[res.x, res.y, res.z] for res in results.left_hand_landmarks.landmark]).flatten(
     ) if results.left_hand_landmarks else np.zeros(21*3)
     rh = np.array([[res.x, res.y, res.z] for res in results.right_hand_landmarks.landmark]).flatten(
     ) if results.right_hand_landmarks else np.zeros(21*3)
-    return np.concatenate([pose, face, lh, rh])
+    return np.concatenate([pose, lh, rh])
 
 
 colors = [(245,117,16), (117,245,16), (16,117,245)]
